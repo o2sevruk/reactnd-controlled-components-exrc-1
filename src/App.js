df804e39a490ab8970f05d1ae4a8a8c2a6508c60
Oleg Sevruk <o2sevruk@gmail.com>
2020-05-24 12:41:48 +0000

    Solution

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }));
+  }
+  
   render() {
+    const {query} = this.state;
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +23,11 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
+          <input type="text" 
+    			 placeholder="Say Something"
+    			 value={query}
+                 onChange={(event) => {this.updateQuery(event.target.value)}}/>
+          <p className="echo">Echo:{query}</p>
           <p>This should mirror the text you typed into the input field.</p>
         </div>
       </div>
